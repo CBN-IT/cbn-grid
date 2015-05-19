96f1aa117584fc51e67e50a9ac20251d2c1745f0
Stancu Florin <niflostancu@gmail.com>
2015-05-19 16:01:38 +0300

    Initial version, based on scss-flex-grid.
    (https://github.com/matthewsimo/scss-flex-grid)

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'dist/flexgrid.css' : 'src/flexgrid.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['sass']);
+};
