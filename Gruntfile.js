1e788feb7b3e07f376671abcc02a97cc2748bd09
Stancu Florin <niflostancu@gmail.com>
2015-05-19 16:35:18 +0300

    Added polymer/web components-usable CSS with /deep/ rules.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,42 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		
 		sass: {
 			options: {
-				sourceMap: true
+				sourceMap: true,
+				sourceComments: false
 			},
 			dist: {
 				files: {
-					'dist/flexgrid.css' : 'src/flexgrid.scss'
+					'dist/flexgrid.css': 'src/flexgrid.scss',
+					'dist/flexgrid-polymer.css': 'src/flexgrid-polymer.scss'
+				}
+			}
+		},
+		cssmin: {
+			options: {
+				shorthandCompacting: false,
+				roundingPrecision: -1
+			},
+			target: {
+				files: {
+					'dist/flexgrid.css': 'dist/flexgrid.css',
+					'dist/flexgrid-polymer.css': 'dist/flexgrid-polymer.css'
 				}
 			}
 		},
 		watch: {
 			css: {
 				files: '**/*.scss',
-				tasks: ['sass']
+				tasks: ['compile']
 			}
 		}
 	});
+
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['sass']);
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+	grunt.registerTask('default', ['sass', 'cssmin'] );
 };
